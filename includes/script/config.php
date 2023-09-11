4cc3a84149718a5ce2cadd4242034094a705003f
mcbsalvacion <markchristian.salvacion@arrowgologistics.com>
2023-09-11 13:16:47 +0800

    First File Upload

---
+++ includes/script/config.php
@@ -0,0 +1,26 @@
+<?php
+
+
+/**
+ * Database Name:
+ * pcdsi_wms
+ */
+
+
+
+
+ if ($_SERVER['SERVER_ADDR'] == '194.163.32.64') { //HOSTINGER 194.163.32.64
+
+	define('DB_HOST', 'localhost');          // Set database host
+	define('DB_USER', 'u478425112_mcbs_nexen');   // Set database user
+	define('DB_PASS', 'AGLsmark689');         // Set database password
+	define('DB_NAME', 'u478425112_nexen'); // Set database name
+} else {
+
+	define('DB_HOST', 'localhost');          // Set database host
+	define('DB_USER', 'root');               // Set database user
+	define('DB_PASS', '');                   // Set database password
+	define('DB_NAME', 'siri_mdc_db2');        // Set database name
+}
+
+
